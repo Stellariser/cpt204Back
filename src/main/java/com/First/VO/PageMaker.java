42ad83944a10f5ce4402b7a05a3c7a1160d1fa0a
Jeongyeong Park <>
2022-03-26 17:08:31 +0800

    Paging(First Draft Code)

---
+++ src/main/java/com/First/VO/PageMaker.java
@@ -0,0 +1,84 @@
+package com.First.VO;
+
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+public class PageMaker {
+    private int totalCount;
+	private int startPage;
+	private int endPage;
+	private boolean prev;
+	private boolean next;
+	private int displayPageNum = 10; //the number of page buttons shown on the bottom of the page
+	private Criteria cri;
+	
+	public void setCri(Criteria cri) {
+		this.cri = cri;
+	}
+	
+	public void setTotalCount(int totalCount) {
+		this.totalCount = totalCount;
+		calcData();
+	}
+	
+	public int getTotalCount() {
+		return totalCount;
+	}
+	
+	public int getStartPage() {
+		return startPage;
+	}
+	
+	public int getEndPage() {
+		return endPage;
+	}
+	
+	public boolean isPrev() {
+		return prev;
+	}
+	
+	public boolean isNext() {
+		return next;
+	}
+	
+	public int getDisplayPageNum() {
+		return displayPageNum;
+	}
+	
+	public Criteria getCri() {
+		return cri;
+	}
+	 
+	private void calcData() {
+        //cri.getPage() : current page num
+        //끝 페이지 번호 = (현재 페이지 번호 / 화면에 보여질 페이지 번호의 갯수) * 화면에 보여질 페이지 번호의 갯수
+		endPage = (int) (Math.ceil(cri.getPage() / (double)displayPageNum) * displayPageNum);
+        //시작 페이지 번호 = (끝 페이지 번호 - 화면에 보여질 페이지 번호의 갯수) + 1
+		startPage = (endPage - displayPageNum) + 1;
+
+	    //cri.getPerPageNum() : the number of posts shown in on page
+        //totalCount : total number of posts
+        //마지막 페이지 번호 = 총 게시글 수 / 한 페이지당 보여줄 게시글의 갯수
+		int tempEndPage = (int) (Math.ceil(totalCount / (double)cri.getPerPageNum()));
+		if (endPage > tempEndPage) {
+			endPage = tempEndPage;
+		}
+        //Create preveious button or not
+        //이전 버튼 생성 여부 = 시작 페이지 번호 == 1 ? false : true
+		prev = startPage == 1 ? false : true;
+        //Create next button or not
+        //다음 버튼 생성 여부 = 끝 페이지 번호 * 한 페이지당 보여줄 게시글의 갯수 < 총 게시글의 수 ? true: false
+		next = endPage * cri.getPerPageNum() >= totalCount ? false : true;
+	}
+	
+	public String makeQuery(int page) {
+		UriComponents uriComponents =
+		UriComponentsBuilder.newInstance()
+						    .queryParam("page", page)
+							.queryParam("perPageNum", cri.getPerPageNum())
+							.build();
+		   
+		return uriComponents.toUriString();
+	}
+    
+}
