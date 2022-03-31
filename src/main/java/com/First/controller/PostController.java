4eaa05299cf6652362b1f198962f9a7f8b78432d
Jeongyeong Park <>
2022-03-31 15:42:54 +0800

    Write a Post(Send a Post) in PostController

--- src/main/java/com/First/controller/PostController.java
+++ src/main/java/com/First/controller/PostController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,8 +19,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import ch.qos.logback.classic.Logger;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/post")
@@ -68,7 +71,43 @@ public class PostController {
         return JSONObject.toJSONString(resultMap);
     }
 
-    //display list of post
+    //Write a post
+    //View Writing a post Page
+	@RequestMapping(value = "/writeView", method = RequestMethod.GET)
+	public void writeView() throws Exception{
+
+	}
+    
+	//Write a post
+	@RequestMapping(value = "/write", method = RequestMethod.POST)
+	public String write(@RequestBody Post newPost) throws Exception{
+        String title = newPost.getTitle();
+        int writerId = newPost.getWriterId();
+        Date writtenTime = newPost.getWrittenTime();
+        String content = newPost.getContent();
+        int anonymous = newPost.getAnonymous();
+        
+        Map<String, Object> map = new HashMap<>();
+        Map<String, Object> postMap = new HashMap<>();
+        
+        //Are we allowed to have the same title?
+        Post post = new Post();
+        post.setTitle(title);
+        post.setWriterId(writerId);
+        post.setWrittenTime(writtenTime);
+        post.setContent(content);
+        post.setAnonymous(anonymous);
+        postService.addPost(post);
+        
+        map.put("id", postService.queryPostByTitle(title).getId());
+        postMap.put("data", map);
+        postMap.put("status", 200);
+        postMap.put("msg", "Successfully Posted.");
+        
+		return JSONObject.toJSONString(postMap);
+	}
+
+    //Display the list of post(Paging)
     @RequestMapping(value = "/list", method = RequestMethod.GET, produces="application/json")
     @ResponseBody
     public PageInfo<Post> list(@RequestParam(value="pageNo", required=false, defaultValue="1")int pageNum,
@@ -80,5 +119,9 @@ public class PostController {
             PageInfo<Post> page = postService.getPostForPage(pageNum,pageSize);
             return  page;
     }
+
+ 
+
+
     
 }
