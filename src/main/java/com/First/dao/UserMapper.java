f0da2def44fa06dbd8153d40fe6c070edf65b921
Zhuo01 <zhuodanni2001@163.com>
2022-04-13 14:02:00 +0800

    user personal info update

--- src/main/java/com/First/dao/UserMapper.java
+++ src/main/java/com/First/dao/UserMapper.java
@@ -24,4 +24,22 @@ public interface UserMapper {
     int updateAvator(User user);
 
     int updatePassword(User user);
+
+    String addGender(String gender);
+
+    String deleteGender(String gender);
+
+    String updateGender(String gender);
+
+    int addGrade(String grade);
+
+    int deleteGrade(String grade);
+
+    int updateGrade(String gender);
+
+    int addMajor(String major);
+
+    int deleteMajor (String major);
+
+    int updateMajor(String major);
 }
